82e8b87b28b99495271767a5ccc6011e2f1f87fc
shufflesbp-real <shufflesbp@gmail.com>
2025-10-10 19:40:34 +0530

    Updated paths in mddial dataloader file

--- mddial-diagnosis/data_loader.py
+++ mddial-diagnosis/data_loader.py
@@ -6,18 +6,18 @@ def load_files():
     """
     Load all the necessary data files.
     """
-    with open("/home/Byomakesh/ours-diagnosis/brishti-didi-files-all/Dataset/MDdial/dict_patient_train.json", "r") as f:
+    with open("/data-files/mddial/dict_patient_train.json", "r") as f:
         dict_patient_train = json.load(f)
-    with open("/home/Byomakesh/ours-diagnosis/brishti-didi-files-all/Dataset/MDdial/dict_patient_test.json", "r") as f:
+    with open("/data-files/mddial/dict_patient_test.json", "r") as f:
         dict_patient_test = json.load(f)
-    with open("/home/Byomakesh/ours-diagnosis/brishti-didi-files-all/Dataset/MDdial/symptom_co-occurence.json", "r") as f:
+    with open("/data-files/mddial/symptom_co-occurence.json", "r") as f:
         co_occurrence_dict = json.load(f)
-    with open('/home/Byomakesh/ours-diagnosis/brishti-didi-files-all/Dataset/MDdial/symptom.txt', 'r') as file:
+    with open('/data-files/mddial/symptom.txt', 'r') as file:
         symptom_list = [line.strip().lower() for line in file]
-    with open("/home/Byomakesh/ours-diagnosis/brishti-didi-files-all/Dataset/MDdial/disease_symptoms.txt") as f:
+    with open("/data-files/mddial/disease_symptoms.txt") as f:
         disease_symptom_dict = json.load(f)
 
-    p_d_given_df = pd.read_csv("/home/Byomakesh/ours-diagnosis/brishti-didi-files-all/Dataset/MDdial/conditional_probabilities_P_D_given_S.csv")
+    p_d_given_df = pd.read_csv("/data-files/mddial/conditional_probabilities_P_D_given_S.csv")
     p_d_given_s = defaultdict(dict)
     for _, row in p_d_given_df.iterrows():
         symptom = row['symptom']
@@ -25,7 +25,7 @@ def load_files():
         prob = row['P(D|S)']
         p_d_given_s[symptom][disease] = prob
 
-    min_scores_df = pd.read_csv("/home/Byomakesh/ours-diagnosis/brishti-didi-files-all/Dataset/MDdial/Mddial_wts_2.csv")
+    min_scores_df = pd.read_csv("/data-files/mddial/Mddial_wts_2.csv")
     min_scores_df = min_scores_df.rename(columns={'weight': 'P(D|S)'})
     min_scores = min_scores_df.groupby('disease')['P(D|S)'].min().to_dict()
     min_scores = {k.capitalize(): v for k, v in min_scores.items()}
